0dcb620ab8f596dcd6f1ba9228a54f2f4f7e7c38
Nelson Chan <chakhong.chan@aecom.com>
2020-08-22 19:10:12 +0800

    Fix _drawnLayers error, format document

--- src/main.js
+++ src/main.js
@@ -1,62 +1,54 @@
-
-
-import rough from '../node_modules/roughjs/src/index'
-
+import rough from "../node_modules/roughjs/bin/rough";
 
 var RoughCanvas = L.Canvas.extend({
-	_initContainer: function () {
+  _initContainer: function () {
+    L.Canvas.prototype._initContainer.call(this);
+    this._rc = rough.canvas(this._container);
+  },
 
-		L.Canvas.prototype._initContainer.call(this);
-		this._rc = rough.canvas(this._container);
-	},
+  _updatePoly: function (layer, closed) {
+    if (!this._drawing) {
+      return;
+    }
 
-	_updatePoly: function (layer, closed) {
+    var parts = layer._parts,
+      len = parts.length,
+      ctx = this._ctx;
 
+    if (!len) {
+      return;
+    }
 
-		if (!this._drawing) { return; }
+    // this._layers[layer._leaflet_id] = layer;
 
-		var i, j, len2, p,
-			parts = layer._parts,
-			len = parts.length,
-			ctx = this._ctx;
+    var options = layer.options;
+    var pathOption = {};
 
-		if (!len) { return; }
+    pathOption.roughness = options.roughness || 1;
+    pathOption.bowing = options.bowing || 1;
+    pathOption.stroke = options.strokeColor || "#000000";
+    pathOption.strokeWidth = options.strokeWidth || 1;
+    if (closed) {
+      pathOption.fill = options.fillColor || options.color;
+      pathOption.fillStyle = options.fillStyle || "";
+      pathOption.fillWeight = options.fillWeight || "";
+      pathOption.hachureAngle = options.hachureAngle || -41;
+      pathOption.hachureGap = options.hachureGap || 4;
+      pathOption.simplification = options.simplification || 0.5;
+    }
+    pathOption.curveStepCount = options.curveStepCount || 9;
 
-		this._drawnLayers[layer._leaflet_id] = layer;
+    var svgPathStr = L.SVG.pointsToPath(parts, closed);
+    this._rc.path(svgPathStr, pathOption);
 
-
-		
-
-		var svgPathStr = L.SVG.pointsToPath(parts, closed);
-		console.log(closed);
-
-		var options = layer.options;
-		var pathOption = {};
-
-
-		pathOption.roughness = options.roughness || 1;
-		pathOption.bowing = options.bowing || 1;
-		pathOption.stroke = options.strokeColor || '#000000';
-		pathOption.strokeWidth = options.strokeWidth || 1;
-		if (closed) {
-			pathOption.fill = options.fillColor || options.color;
-			pathOption.fillStyle = options.fillStyle || '';
-			pathOption.fillWeight = options.fillWeight || '';
-			pathOption.hachureAngle = options.hachureAngle || -41;
-			pathOption.hachureGap = options.hachureGap || 4;
-			// pathOption.simplification = options.simplification || 1;
-		}
-		pathOption.curveStepCount = options.curveStepCount || 9;
-				
-		this._rc.path(svgPathStr, pathOption);
-
-		
-
-	},
-})
+    layer.options.fillOpacity = 0;
+    // layer.options.opacity = 0;
+    L.Canvas.prototype._updatePoly.call(this, layer, closed);
+  },
+});
 
 L.Canvas.RoughCanvas = RoughCanvas;
 
 L.Canvas.roughCanvas = () => {
-	return new RoughCanvas();
-}
+  return new RoughCanvas();
+};
