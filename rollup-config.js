10213ee9f2fc2844bc8d6fa063f6b4c4df781fbe
Nelson Chan <chakhong.chan@aecom.com>
2020-10-06 18:33:45 +0800

    I don't really know what I'm doing

--- rollup-config.js
+++ rollup-config.js
@@ -1,20 +1,26 @@
-import { getBabelOutputPlugin } from "@rollup/plugin-babel";
+import { babel } from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
   input: "src/main.js",
-  output: {
-    file: "dist/leaflet-roughcanvas.js",
-    format: "esm",
-    sourcemap: true,
-  },
+  output: [
+    {
+      file: "dist/leaflet-roughcanvas.js",
+      format: "umd",
+      sourcemap: true,
+    },
+  ],
   plugins: [
-    getBabelOutputPlugin({
+    babel({
+      babelHelpers: "bundled",
+      exclude: /node_modules/,
       presets: [
         [
           "@babel/preset-env",
           {
-            modules: "umd",
+            modules: false,
             useBuiltIns: "usage",
             corejs: 3,
             targets: {
@@ -23,13 +29,11 @@ export default {
           },
         ],
       ],
-      plugins: [
-        [
-          "@babel/plugin-transform-runtime",
-          { useESModules: false, regenerator: true },
-        ],
-      ],
     }),
     terser(),
+    commonjs(),
+    nodeResolve({
+      browser: true,
+    }),
   ],
 };
