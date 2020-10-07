7cdf69a3f36de3f29156b902281817843db1387f
Nelson Chan <chakhong.chan@aecom.com>
2020-10-07 18:31:14 +0800

    Revert to old rollup config

--- rollup-config.js
+++ rollup-config.js
@@ -1,26 +1,20 @@
-import { babel } from "@rollup/plugin-babel";
+import { getBabelOutputPlugin } from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 
 export default {
   input: "src/main.js",
-  output: [
-    {
-      file: "dist/leaflet-roughcanvas.js",
-      format: "umd",
-      sourcemap: true,
-    },
-  ],
+  output: {
+    file: "dist/leaflet-roughcanvas.js",
+    format: "esm",
+    sourcemap: true,
+  },
   plugins: [
-    babel({
-      babelHelpers: "bundled",
-      exclude: /node_modules/,
+    getBabelOutputPlugin({
       presets: [
         [
           "@babel/preset-env",
           {
-            modules: false,
+            modules: "umd",
             useBuiltIns: "usage",
             corejs: 3,
             targets: {
@@ -29,11 +23,13 @@ export default {
           },
         ],
       ],
+      plugins: [
+        [
+          "@babel/plugin-transform-runtime",
+          { useESModules: false, regenerator: true },
+        ],
+      ],
     }),
     terser(),
-    commonjs(),
-    nodeResolve({
-      browser: true,
-    }),
   ],
 };
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ var RoughCanvas = L.Canvas.extend({
   _initContainer: function () {
     L.Canvas.prototype._initContainer.call(this);
     this._rc = rough.canvas(this._container);
+    this._drawnLayers = {};
   },
 
   _updatePoly: function (layer, closed) {
@@ -12,13 +13,19 @@ var RoughCanvas = L.Canvas.extend({
       return;
     }
 
-    var parts = layer._parts,
+    var i,
+      j,
+      len2,
+      p,
+      parts = layer._parts,
       len = parts.length,
       ctx = this._ctx;
 
     if (!len) {
       return;
     }
+
+    this._drawnLayers[layer._leaflet_id] = layer;
 
     var options = layer.options;
     var pathOption = {};
@@ -36,13 +43,9 @@ var RoughCanvas = L.Canvas.extend({
       pathOption.simplification = options.simplification || 0.5;
     }
     pathOption.curveStepCount = options.curveStepCount || 9;
-
+    
     var svgPathStr = L.SVG.pointsToPath(parts, closed);
     this._rc.path(svgPathStr, pathOption);
-
-    layer.options.fillOpacity = 0;
-    // layer.options.opacity = 0;
-    L.Canvas.prototype._updatePoly.call(this, layer, closed);
   },
 });
 
